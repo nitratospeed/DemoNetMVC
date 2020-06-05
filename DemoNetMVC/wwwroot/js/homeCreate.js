d21f1094c98b432f067cafe8245f74332eca89d0
Eduardo Rojas <nitratospeed@gmail.com>
2020-06-05 18:28:56 -0500

    Add project files.

---
+++ DemoNetMVC/wwwroot/js/homeCreate.js
@@ -0,0 +1,35 @@
+﻿document.getElementById('btn-crear').onclick = function nuevoUsuario() {
+    let form = document.getElementById("create-usuario-form");
+    let formData = new FormData(form);
+
+    if (formData.get("celular").length > 9) {
+        alert("El campo Celular tiene mas de 9 digitos");     
+        return;
+    }
+
+    fetch('http://localhost:5090/api/usuario', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            nombres: formData.get("nombres"),
+            edad: parseInt(formData.get("edad")),
+            ocupacion: formData.get("ocupacion"),
+            celular: formData.get("celular"),
+            direccion: formData.get("direccion"),
+            activo: true
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            window.location.href = "/Home/Index/";
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            document.getElementById("alert-form").style.display = "block";
+        });
+};
+
+document.getElementById('create-usuario-form').onclick = function () { document.getElementById("alert-form").style.display = "none";  };
---
+++ DemoNetMVC/wwwroot/js/homeIndex.js
@@ -0,0 +1,34 @@
+﻿window.onload = function () {
+    fetch('http://localhost:5090/api/usuario', {
+    })
+        .then(response => response.json())
+        .then(data => {
+            let result = data.data;
+
+            var tableRef = document.getElementById('lista-usuarios').getElementsByTagName('tbody')[0];
+
+            result.forEach(function (entry) {
+                var newRow = tableRef.insertRow();
+                var i = 0;
+                for (let [key, value] of Object.entries(entry)) {
+                    var newCell = newRow.insertCell(i);
+
+                    if (key == "activo") {
+                        if (value == true) {
+                            value = "Habilitado";
+                        }
+                        else {
+                            value = "Deshabilitado";
+                        }
+                    }
+
+                    var newText = document.createTextNode(value);
+                    newCell.appendChild(newText);
+                    i++;
+                }
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+};
